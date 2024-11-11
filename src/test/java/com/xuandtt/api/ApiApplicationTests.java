18691b97bbd2abaf0250275dfbaabb4347b4136e
DongThiThanhXuan <dongthithanhxuan.hn@gmail.com>
2024-11-11 14:26:18 +0700

    Initial commit

---
+++ src/test/java/com/xuandtt/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuandtt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
